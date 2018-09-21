ac37a311bf2128c8c92a2b6853c7047b7e342dc3
mharis05 <harissaleemkhi@gmail.com>
2018-09-21 15:26:07 +0400

    Finalized Tests

--- src/test/java/com/zuulDemo/SpringBootWebApplicationTests.java
+++ src/test/java/com/zuulDemo/SpringBootWebApplicationTests.java
@@ -1,18 +1,25 @@
 package com.zuulDemo;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
 import com.zuulDemo.model.Image;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
+import org.apache.commons.codec.binary.Base64;
+import org.assertj.core.api.SoftAssertions;
 import org.json.JSONException;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.io.IOException;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
@@ -21,25 +28,59 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 public class SpringBootWebApplicationTests {
 
     private RequestSpecification request = RestAssured.with();
+    private Image imageResponse;
+    private SoftAssertions softly = new SoftAssertions();
     private Response response;
 
     @Autowired
-    private Image image;
+    private Image imageRequest;
 
     @Before
-    public void createObjects() throws JSONException {
+    public void createObjects() throws JSONException, IOException {
         request.given().baseUri("http://localhost:8081/api/")
-        .contentType(ContentType.JSON);
+                .contentType(ContentType.JSON);
+
+        //Send POST request
+        response = sendRequest(imageRequest, "/image");
+
+        // Deserialize response object
+        imageResponse = new ObjectMapper().readValue(response.asString(), Image.class);
+
     }
-    
+
+    // Verify "test" value after passing demo proxy is base 64 encoded
     @Test
-    public void testImageEncoding() throws Exception {
+    public void testImageEncoding() {
 
-        String requestBody ="{\"image\":\"test\"}";
-        response = request.when().body(requestBody).post("/image");
-        image = new ObjectMapper().readValue(response.asString(), Image.class);
-        System.out.println("RESPONSE IS: " + image.getImage());
+        // Validate that value of "image" in response is indeed "test"
+        String actualValue = imageResponse.getImage();
+        boolean isBase64 = Base64.isArrayByteBase64(actualValue.getBytes());
+        softly.assertThat(isBase64).isTrue();
+    }
+
+    @Test
+    public void testImageHasCorrectValue() {
+
+        String actualBase64Value = imageResponse.getImage();
+        String actualStringValue = new String(Base64.decodeBase64(actualBase64Value.getBytes()));
+        softly.assertThat(actualStringValue).isEqualTo(imageRequest.getImage());
+    }
+
+    @After
+    public void performAsserts() {
+        softly.assertAll();
+    }
 
 
+    public Response sendRequest(Image imageRequest, String endpoint) throws JsonProcessingException {
+        Response response = null;
+
+        // Set values in member variables to create Request POJO
+        imageRequest.setImage("test");
+
+        // Serialize Image class and send POST request to /image Endpoint and save response
+        ObjectWriter writer = new ObjectMapper().writer().withDefaultPrettyPrinter();
+        response = request.when().body(writer.writeValueAsString(imageRequest)).post(endpoint);
+        return response;
     }
 }
